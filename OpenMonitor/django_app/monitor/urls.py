e0fa30b4caefd7ad75ebe45668752da3faf79bf1
Juan Ignacio Villegas Llano <vlljuan99@gmail.com>
2025-07-30 20:49:37 +0200

    Add backend usage README

--- OpenMonitor/django_app/monitor/urls.py
+++ OpenMonitor/django_app/monitor/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
+
+router = DefaultRouter()
+router.register('projects', views.ProjectViewSet, basename='projects')
 
 urlpatterns = [
     path('ping/', views.PingView.as_view(), name='ping'),
     path('openproject/', views.OpenProjectStatusView.as_view(), name='openproject'),
+    path('sync/', views.SyncView.as_view(), name='sync'),
     path('token-auth/', obtain_auth_token, name='token_auth'),
+    path('', include(router.urls)),
 ]
